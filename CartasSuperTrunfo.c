80b642ea84ea9845eb4738f3a6b70c903ea0f5c3
Luizedu13 <luizedu.pacheco@gmail.com>
2025-02-27 15:12:09 -0300

    Update CartasSuperTrunfo.c
    Adicionado if / else para o tema 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Variáveis usadas no programa
-    char cidade1[30], cidade2[30];
+    char estado1[30], estado2[30], cidade1[30], cidade2[30];
     int turistico1, turistico2;
     unsigned long int populacao1, populacao2;
     float area1, area2, pib1, pib2, dens1, dens2, ppc1, ppc2, sp1, sp2;
@@ -14,8 +14,17 @@ int main() {
 
     // Cadastro das informações da Carta 01
     printf("\nCarta 1:\n");
-    printf("Estado: A\n");
-    printf("Código: A01\n");
+
+     /* 
+      Solicita a cidade usando fgets para ler até o final, pois quando usei o Scanf ao colocar "São Paulo" ele não lia até o final, 
+      tive que procurar outra saida na internet para conseguir ler toda a linha e não levar lixo para a próxima.
+    */
+    printf("Escreva o Estado Abreviado (XX):\n");
+    fgets(estado1, sizeof(estado1), stdin);
+    estado1[strcspn(estado1, "\n")] = 0;
+
+    // Mostra o código junto com o Estado
+    printf("Código: %s (01)\n", estado1);
 
     /* 
       Solicita a cidade usando fgets para ler até o final, pois quando usei o Scanf ao colocar "São Paulo" ele não lia até o final, 
@@ -51,10 +60,19 @@ int main() {
     // Limpeza
     getchar();
 
-    // Cadastro das informações da Carta 02
-    printf("\n\nCarta 2:\n");
-    printf("Estado: B\n");
-    printf("Código: B02\n");
+    // Cadastro das informações da Carta 01
+    printf("\nCarta 2:\n");
+
+    /* 
+      Solicita a cidade usando fgets para ler até o final, pois quando usei o Scanf ao colocar "São Paulo" ele não lia até o final, 
+      tive que procurar outra saida na internet para conseguir ler toda a linha e não levar lixo para a próxima.
+    */
+    printf("Escreva o Estado Abreviado (XX):\n");
+    fgets(estado2, sizeof(estado2), stdin);
+    estado2[strcspn(estado2, "\n")] = 0;
+
+    // Mostra o código junto com o Estado
+    printf("Código: %s (02)\n", estado2);
 
     // Solicita a cidade usando fgets para le	qr até o final, pois quando usei o Scanf ao colocar "São Paulo" ele não lia até o final.
     printf("Escreva a Cidade:\n");
@@ -87,45 +105,69 @@ int main() {
     // Limpeza
     getchar();
 
-    // Imprime os dados para cabeçalho
-    printf("\n\n-------------------------------------\n");
-    printf("          Cartas Cadastradas\n");
-    printf("-------------------------------------\n");
-
-    // Exibe as informações preenchidas anteriormente na Carta 01
-    printf("\nCarta 1:\n"); // Mostra a Carta 1
-    printf("Estado: A\n"); // Mostra o Estado A
-    printf("Código: A01\n"); // Mostra o Código
-    printf("Nome da Cidade: %s\n", cidade1); // Mostra a cidade cadastrada
-    printf("População: %.2ld\n", populacao1); // Mostra a população cadastrada
-    printf("Área: %.2f Km²\n", area1);  // Mostra área cadastrada com 2 casas decimais 
-    printf("PIB: %.2f bilhões de Reais\n", pib1);  // Mostra o PIB cadastrado com 2 casas decimais
-    printf("Número de Pontos Turísticos: %d\n", turistico1);  // Mostra a Quantidade de pontos turísticos cadastrado
-
-
-    // Exibe as informações preenchidas anteriormente na Carta 02
-    printf("\nCarta 2:\n"); // Mostra a Carta 2
-    printf("Estado: B\n"); // Mostra Estado B
-    printf("Código: A02\n"); // Mostra o Código 
-    printf("Nome da Cidade: %s\n", cidade2); // Mostra a cidade cadastrada
-    printf("População: %.2ld\n", populacao2); // Mostra a população cadastrada
-    printf("Área: %.2f Km²\n", area2);  // Mostra área cadastrada com 2 casas decimais 
-    printf("PIB: %.2f bilhões de Reais\n", pib2);  // Mostra o PIB cadastrado com 2 casas decimais
-    printf("Número de Pontos Turísticos: %d\n", turistico2);  // Mostra a Quantidade de pontos turísticos cadastrado
-
     // Imprime os dados para cabeçalho para Comparações das cartas
     printf("\n\n-------------------------------------\n");
-    printf("          Comparação de Cartas\n");
+    printf("  Comparação das Cartas (01) e (02) \n");
     printf("-------------------------------------\n\n");
 
-    // Imprime os dados para comparação das cartas e mostra as informações de qual venceu
-    printf("População: Carta %d venceu (%d)\n", populacao1 > populacao2 ? 1 : 2, populacao1 > populacao2);
-    printf("Área: Carta %d venceu (%d)\n", area1 > area2 ? 1 : 2, area1 > area2);
-    printf("PIB: Carta %d venceu (%d)\n", pib1 > pib2 ? 1 : 2, pib1 > pib2);
-    printf("Pontos Turísticos: Carta %d venceu (%d)\n", turistico1 > turistico2 ? 1 : 2, turistico1 > turistico2);
-    printf("Densidade Populacional: Carta %d venceu (%d)\n", dens1 < dens2 ? 1 : 2, dens1 < dens2);
-    printf("PIB per Capita: Carta %d venceu (%d)\n", ppc1 > ppc2 ? 1 : 2, ppc1 > ppc2);
-    printf("Super Poder: Carta %d venceu (%d)\n", sp1 > sp2 ? 1 : 2, sp1 > sp2);
+    // Estrutura composta para verificar qual carta venceu
+    if (populacao1 > populacao2) {
+        printf("    Carta 1 - %s %s: %.2ld\n", cidade1, estado1, populacao1);
+        printf("    Carta 2 - %s %s: %.2ld\n", cidade2, estado2, populacao2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2ld\n", cidade2, estado2, populacao2);
+        printf("    Carta 1 - %s %s: %.2ld\n", cidade1, estado1, populacao1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (area1 > area2) {
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, area1);
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, area2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, area2);
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, area1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (pib1 > pib2) {
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, pib1);
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, pib2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, pib2);
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, pib1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (turistico1 > turistico2) {
+        printf("    Carta 1 - %s %s: %d\n", cidade1, estado1, turistico1);
+        printf("    Carta 2 - %s %s: %d\n", cidade2, estado2, turistico2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %d\n", cidade2, estado2, turistico2);
+        printf("    Carta 1 - %s %s: %d\n", cidade1, estado1, turistico1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (dens1 > dens2) {
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, dens1);
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, dens2);
+        printf(" ***Carta 1 - %s Venceu!***\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, dens2);
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, dens1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (ppc1 > ppc2) {
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, ppc1);
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, ppc2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, ppc2);
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, ppc1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    } if (sp1 > sp2) {
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, sp1);
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, sp2);
+        printf(" ***Carta 1 - %s Venceu!***\n\n", cidade1);
+    } else {
+        printf("    Carta 2 - %s %s: %.2f\n", cidade2, estado2, sp2);
+        printf("    Carta 1 - %s %s: %.2f\n", cidade1, estado1, sp1);
+        printf(" ***Carta 2 - %s Venceu!***\n\n", cidade2);
+    }
 
     return 0;
 }
